1fb06a0fa684e5fb9fabc1332af093da85b82d27
Nedheesh Hasija <nedheesh.hasija@gmail.com>
2020-10-27 13:03:22 +0530

    Initial commit

---
+++ app/android/app/src/main/kotlin/com/example/topnote/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.topnote
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ui/README.md
@@ -0,0 +1,3 @@
+## UI image taken from Dribbble
+
+Credits to Slava Kutovoy: https://dribbble.com/shots/10858297-Notes-App?_=1603781099414
