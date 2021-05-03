61e31848701faf75afd4049c80adb8c99eacf53e
Nedheesh Hasija <nedheesh.hasija@gmail.com>
2021-05-03 16:41:18 +0530

    Formatted the multiline content to display within a single line on note list page

--- topnotes/lib/ui/screens/notes_list_screen.dart
+++ topnotes/lib/ui/screens/notes_list_screen.dart
@@ -44,14 +44,20 @@ class _NotesListScreenState extends State<NotesListScreen> {
     return "${timeStamp.day} ${monthsInYear[timeStamp.month]}, ${timeStamp.year}";
   }
 
+  String displayContent(String content) {
+    if (content.contains('\n')) {
+      var str = content.replaceAll('\n', ' ');
+      return str;
+    }
+
+    return content;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.only(top: MediaQuery
-            .of(context)
-            .padding
-            .top),
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -76,125 +82,122 @@ class _NotesListScreenState extends State<NotesListScreen> {
                 child: widget.notesToBeDisplayed.length == 0
                     ? EmptyStateWidget()
                     : GridView.builder(
-                  reverse: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: widget.notesToBeDisplayed.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 1,
-                    mainAxisSpacing: SizeConfig.blockSizeVertical * 2,
-                    crossAxisSpacing: SizeConfig.blockSizeHorizontal * 2,
-                    childAspectRatio: 3.0,
-                  ),
-                  itemBuilder: (context, index) =>
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              CupertinoPageRoute(
-                                builder: (context) =>
-                                    NotePage(
-                                      note: widget.notesToBeDisplayed[index],
+                        reverse: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: widget.notesToBeDisplayed.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 1,
+                          mainAxisSpacing: SizeConfig.blockSizeVertical * 2,
+                          crossAxisSpacing: SizeConfig.blockSizeHorizontal * 2,
+                          childAspectRatio: 3.0,
+                        ),
+                        itemBuilder: (context, index) => GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                  builder: (context) => NotePage(
+                                    note: widget.notesToBeDisplayed[index],
+                                  ),
+                                ));
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white10,
+                              borderRadius: BorderRadius.circular(
+                                  SizeConfig.blockSizeHorizontal),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: SizeConfig.blockSizeHorizontal * 5,
+                              vertical: SizeConfig.blockSizeVertical * 2.5,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                      bottom: SizeConfig.blockSizeHorizontal),
+                                  child: Container(
+                                    child: Text(
+                                      "${widget.notesToBeDisplayed[index].title}",
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize:
+                                              SizeConfig.blockSizeVertical * 2),
                                     ),
-                              ));
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white10,
-                            borderRadius: BorderRadius.circular(
-                                SizeConfig.blockSizeHorizontal),
-                          ),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: SizeConfig.blockSizeHorizontal * 5,
-                            vertical: SizeConfig.blockSizeVertical * 2.5,
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(
-                                    bottom: SizeConfig.blockSizeHorizontal),
-                                child: Container(
-                                  child: Text(
-                                    "${widget.notesToBeDisplayed[index].title}",
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize:
-                                        SizeConfig.blockSizeVertical * 2),
                                   ),
                                 ),
-                              ),
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Text(
-                                    "${displayTime(
-                                      widget.notesToBeDisplayed[index]
-                                          .timeStamp,
-                                    )}",
-                                    overflow: TextOverflow.fade,
-                                    style: TextStyle(color: Colors.white38),
-                                  ),
-                                  Expanded(
-                                    child: Padding(
-                                      padding: EdgeInsets.only(
-                                          left:
-                                          SizeConfig.blockSizeHorizontal *
-                                              3),
-                                      child: Container(
-                                        child: Text(
-                                          "${widget.notesToBeDisplayed[index]
-                                              .content}",
-                                          overflow: TextOverflow.ellipsis,
-                                          maxLines: 1,
-                                          style: TextStyle(
-                                              color: Colors.white30),
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                      "${displayTime(
+                                        widget.notesToBeDisplayed[index]
+                                            .timeStamp,
+                                      )}",
+                                      overflow: TextOverflow.fade,
+                                      style: TextStyle(color: Colors.white38),
+                                    ),
+                                    Expanded(
+                                      child: Padding(
+                                        padding: EdgeInsets.only(
+                                            left:
+                                                SizeConfig.blockSizeHorizontal *
+                                                    3),
+                                        child: Container(
+                                          child: Text(
+                                            "${displayContent(widget.notesToBeDisplayed[index].content)}", 
+                                            overflow: TextOverflow.ellipsis,
+                                            maxLines: 1,
+                                            style: TextStyle(
+                                                color: Colors.white60),
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              SizedBox(
-                                height: SizeConfig.blockSizeVertical * 2,
-                              ),
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  for (var tag in widget
-                                      .notesToBeDisplayed[index]
-                                      .associatedTags)
-                                    Container(
-                                      padding: EdgeInsets.all(
-                                          SizeConfig.blockSizeHorizontal),
-                                      decoration: BoxDecoration(
-                                          color: Color(0xFF687581),
-                                          borderRadius: BorderRadius.circular(
-                                              SizeConfig.blockSizeVertical *
-                                                  10)),
-                                      child: Text(
-                                        "#${tag.tagName}",
-                                        style:
-                                        TextStyle(color: Colors.white70),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: SizeConfig.blockSizeVertical * 2,
+                                ),
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    for (var tag in widget
+                                        .notesToBeDisplayed[index]
+                                        .associatedTags)
+                                      Container(
+                                        padding: EdgeInsets.all(
+                                            SizeConfig.blockSizeHorizontal),
+                                        decoration: BoxDecoration(
+                                            color: Color(0xFF687581),
+                                            borderRadius: BorderRadius.circular(
+                                                SizeConfig.blockSizeVertical *
+                                                    10)),
+                                        child: Text(
+                                          "#${tag.tagName}",
+                                          style:
+                                              TextStyle(color: Colors.white70),
+                                        ),
                                       ),
-                                    ),
-                                  Spacer(),
-                                  Icon(
-                                    Icons.star,
-                                    color: widget.notesToBeDisplayed[index]
-                                        .isFavorite ==
-                                        true
-                                        ? Colors.yellowAccent
-                                        : Colors.white10,
-                                  )
-                                ],
-                              )
-                            ],
+                                    Spacer(),
+                                    Icon(
+                                      Icons.star,
+                                      color: widget.notesToBeDisplayed[index]
+                                                  .isFavorite ==
+                                              true
+                                          ? Colors.yellowAccent
+                                          : Colors.white10,
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                ),
               ),
             ],
           ),
